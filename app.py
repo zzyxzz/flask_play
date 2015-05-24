b1dc1726a930a741b354cbdb0a24e20f7c72e7d9
Shawn Yang <yxzz@hotmail.com>
2015-05-24 13:24:56 +0100

    add models to app

--- app.py
+++ app.py
@@ -1,17 +1,91 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask_flatpages import FlatPages, pygments_style_defs, pygmented_markdown
-import collections
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 FLATPAGES_EXTENSION = ".md"
 FLATPAGES_ROOT = "content"
 POSTS_DIR = "posts"
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 flatpages = FlatPages(app)
 app.config.from_object(__name__)
 post_list = []
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 't2_data.sqlite')
+app.config['SQLAlchemy_COMMIT_ON_TEARDOWN'] = True
+db = SQLAlchemy(app)
+
+"""
+Define models according to the table in csv file 
+for import data from csv file or furthur database manipulations.
+"""
+class Organisation(db.Model):
+	__tablename__ = 'organisations'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(80), unique = True)
+	town_id = db.Column(db.Integer, db.ForeignKey('towns.id'))
+	sub_tier_id = db.Column(db.Integer, db.ForeignKey('subtiers.id'))
+
+	def __init__(self, name):
+		self.name = name
+
+	def __repr__(self):
+		return '<Organisation %r>' % self.name
+
+class Town(db.Model):
+	__tablename__ = 'towns'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(80), unique = True)
+	county_id = db.Column(db.Integer, db.ForeignKey('counties.id'))
+	organisations = db.relationship('Organisation', backref = 'town', lazy = 'dynamic')
+
+	def __init__(self, name):
+		self.name = name
+
+	def __repr__(self):
+		return '<Town %r>' % self.name
+
+class County(db.Model):
+	__tablename__ = 'counties'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(80), unique = True)
+	towns = db.relationship('Town', backref = 'county', lazy = 'dynamic')
+
+	def __init__(self, name):
+		self.name = name
+
+	def __repr__(self):
+		return '<County %r>' % self.name
+
+class Tier(db.Model):
+	__tablename__ = 'tiers'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(80), unique = True)
+	sub_tiers = db.relationship('SubTier', backref = 'tier', lazy = 'dynamic')
+
+	def __init__(self, name):
+		self.name = name
+
+	def __repr__(self):
+		return '<Tier %r>' % self.name
+
+class SubTier(db.Model):
+	__tablename__ = 'subtiers'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(80), unique = True)
+	tier_id = db.Column(db.Integer, db.ForeignKey('tiers.id'))
+	organisations = db.relationship('Organisation', backref = 'subtier', lazy = 'dynamic')
+
+	def __init__(self, name):
+		self.name = name
+		
+	def __repr__(self):
+		return '<Sub Tier %r>' % self.name
 
 @app.route("/")
 def index():
